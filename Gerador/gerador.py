5e398f204407c6627933607910477cae8e61742a
Joao vitor <gomesjoaovitor222@gmail.com>
2022-09-18 10:57:50 -0300

    Gerador senhas

--- Gerador/gerador.py
+++ Gerador/gerador.py
@@ -1,15 +1,30 @@
+import json
 import random
+
+
 
 minu = "abcdefghijklmnopqrstuvwxyz"
 maiu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 num = "0123456789"
 simbolos = "[]{}()*#$%;/_-"
 
-qntd = int(input("Digite o tamanho da sua senha: "))
-qntdInt = int(qntd)
-length = qntdInt
+pergunta = int(input("Quantas senhas deseja gerar? "))
 
-all = minu + maiu + num + simbolos
-passwordAll = "".join(random.sample(all, length))
+for i in range(pergunta):
 
-print(passwordAll)
+    Lugar_senha = input("Qual o lugar que você quer gerar a senha? ")
+
+    qntd = int(input("Digite o tamanho da sua senha: "))
+    qntdInt = int(qntd)
+    length = qntdInt
+
+    all = minu + maiu + num + simbolos
+    passwordAll = "".join(random.sample(all, length))
+
+    print(passwordAll)
+
+
+with open('gerador.json', 'w') as json_file:
+    json.dump(Lugar_senha, json_file) 
+    json.dump(passwordAll, json_file)
+
